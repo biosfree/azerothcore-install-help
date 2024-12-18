38634f2c31ec5d2b80a32ccf34808e9104fdc482
Bite Of Storm <57522664+biosfree@users.noreply.github.com>
2024-12-18 10:28:08 +0300

    Update motd.sql

--- lua-and-sql/sql/motd.sql
+++ lua-and-sql/sql/motd.sql
@@ -1,1 +1,2 @@
-INSERT INTO `motd` (`realmid`, `text`) VALUES (1, 'Добро пожаловать на World of Warcraft сервер "Шторм"');
+DELETE FROM `motd` WHERE `realmid`=1; INSERT INTO `motd` (`realmid`, `text`) VALUES (1, 'Welcome to an AzerothCore server "Storm of Blades"');
+DELETE FROM `motd_localized` WHERE realmid=1; INSERT INTO `motd_localized` (`realmid`, `locale`, `text`) VALUES (1, 'ruRU','Добро пожаловать на World of Warcraft сервер "Шторм клинков"');
